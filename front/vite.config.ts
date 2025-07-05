39e106e322b11bc504c582ab986a6b14eb5ad53d
JianMing Dong <dong_j_m@163.com>
2025-07-05 15:21:52 +0800

    🎉 init(all): 初始化vue3项目、nodejs项目

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [vue(), vueDevTools()],
-    // build: {
-    //     sourcemap: true,
-    // },
-})
+  plugins: [vue(), vueDevTools()],
+  server: {
+    host: "0.0.0.0",
+    port: 5555,
+    strictPort: true,
+  },
+  // build: {
+  //     sourcemap: true,
+  // },
+});
